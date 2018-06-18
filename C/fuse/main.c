e594dc7478a2966455f109b3173504fbe3be2352
Tony Butler <spudz76@gmail.com>
2018-06-17 22:58:08 -0600

    Move CURL handle/headers/etc into substructure of State

--- C/fuse/main.c
+++ C/fuse/main.c
@@ -46,18 +46,22 @@ void sync();
 void sync(){ fflush(stdout); fflush(stderr); }
 
 #define CONFIGFILE "config.json"
+struct CURLStruct {
+    CURL *handle;
+    struct curl_slist *headers;
+};
 struct StateStruct {
     struct json_object *cfg;
     char baseurl[255];
     char username[64];
     char password[64];
-    CURL *curl;
-    struct curl_slist *headers;
+    struct CURLStruct curl;
 };
 void
 InitState(void *userp) {
     struct StateStruct *state = (struct StateStruct *) userp;
-    state->headers = NULL;
+    state->curl.handle = NULL;
+    state->curl.headers = NULL;
 
     struct stat st;
     if(stat(CONFIGFILE, &st) != 0){
@@ -102,6 +106,7 @@ InitState(void *userp) {
     CFG_GET_STRING(password);
 }
 
+#define PAGESIZE 4096
 struct MemoryStruct {
     char *memory;
     size_t page;
@@ -109,8 +114,6 @@ struct MemoryStruct {
     size_t ptr;
     struct json_object *parsed;
 };
-
-#define PAGESIZE 4096
 void
 InitMemory(void *userp) {
     struct MemoryStruct *mem = (struct MemoryStruct *)userp;
@@ -188,36 +191,36 @@ int main(int argc, char *argv[]){
     curl_global_init(CURL_GLOBAL_ALL);
 
     /* get a curl handle */
-    S.curl = curl_easy_init();
-    if(S.curl) {
-        curl_easy_setopt(S.curl, CURLOPT_USERAGENT, "sfs-fuse/1.0");
-        //curl_easy_setopt(S.curl, CURLOPT_VERBOSE, 1L);
-        curl_easy_setopt(S.curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    S.curl.handle = curl_easy_init();
+    if(S.curl.handle){
+        curl_easy_setopt(S.curl.handle, CURLOPT_USERAGENT, "sfs-fuse/1.0");
+        //curl_easy_setopt(S.curl.handle, CURLOPT_VERBOSE, 1L);
+        curl_easy_setopt(S.curl.handle, CURLOPT_SSL_VERIFYPEER, 0L);
 
         /* Headers */
-        S.headers = curl_slist_append(S.headers, "Accept: application/json");
-        S.headers = curl_slist_append(S.headers, "Content-Type: application/json");
-        curl_easy_setopt(S.curl, CURLOPT_HTTPHEADER, S.headers);
+        S.curl.headers = curl_slist_append(S.curl.headers, "Accept: application/json");
+        S.curl.headers = curl_slist_append(S.curl.headers, "Content-Type: application/json");
+        curl_easy_setopt(S.curl.handle, CURLOPT_HTTPHEADER, S.curl.headers);
 
         /* we pass our 'chunk' struct to the callback function */
         struct MemoryStruct chunk;
         InitMemory((void *)&chunk);
-        curl_easy_setopt(S.curl, CURLOPT_WRITEDATA, (void *)&chunk);
+        curl_easy_setopt(S.curl.handle, CURLOPT_WRITEDATA, (void *)&chunk);
         /* send all data to this callback */
-        curl_easy_setopt(S.curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+        curl_easy_setopt(S.curl.handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
 
         char url[512];
         sprintf(url,"%s/user/login",S.baseurl);
-        curl_easy_setopt(S.curl, CURLOPT_URL, url);
-        curl_easy_setopt(S.curl, CURLOPT_REFERER, url); //"http://localhost:8160/");
+        curl_easy_setopt(S.curl.handle, CURLOPT_URL, url);
+        curl_easy_setopt(S.curl.handle, CURLOPT_REFERER, url); //"http://localhost:8160/");
         /* POST data */
-        curl_easy_setopt(S.curl, CURLOPT_POST, 1L);
+        curl_easy_setopt(S.curl.handle, CURLOPT_POST, 1L);
         json_object *json;
         json = json_object_new_object();
         json_object_object_add(json, "tokenType", json_object_new_string("permanent"));
         json_object_object_add(json, "username", json_object_new_string(S.username));
         json_object_object_add(json, "password", json_object_new_string(S.password));
-        curl_easy_setopt(S.curl, CURLOPT_POSTFIELDS, json_object_to_json_string_ext(json,JSON_C_TO_STRING_PLAIN));
+        curl_easy_setopt(S.curl.handle, CURLOPT_POSTFIELDS, json_object_to_json_string_ext(json,JSON_C_TO_STRING_PLAIN));
 
         /* do the thing */
 #ifdef DEBUG
@@ -225,7 +228,7 @@ int main(int argc, char *argv[]){
         sync();
 #endif
         CURLcode res;
-        res = curl_easy_perform(S.curl);
+        res = curl_easy_perform(S.curl.handle);
 
         /* Check for errors */
         if(res != CURLE_OK){
@@ -242,8 +245,8 @@ int main(int argc, char *argv[]){
 #endif
 
         /* always cleanup */
-        curl_slist_free_all(S.headers);
-        curl_easy_cleanup(S.curl);
+        curl_slist_free_all(S.curl.headers);
+        curl_easy_cleanup(S.curl.handle);
     }
     curl_global_cleanup();
     return 0;
